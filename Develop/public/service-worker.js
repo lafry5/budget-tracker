5b291f8b46e05cc157f78b66fe0a71457b3dbbf4
lafry5 <lafry5@yahoo.com>
2020-10-01 20:20:07 -0700

    misc updates to attempt to get it to run

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,6 +1,6 @@
 
 const APP_PREFIX = 'Budget-Tracker';     
-const VERSION = 'version_01';
+const VERSION = 'v1';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const DATA_CACHE_NAME = "data-cache-" + VERSION;
@@ -17,20 +17,24 @@ const FILES_TO_CACHE = [
     "./icons/icon-144x144.png",
     "./icons/icon-152x152.png",
     "./icons/icon-192x192.png",
-
     "./icons/icon-384x384.png",
     "./icons/icon-512x512.png"
   ];
 
-  self.addEventListener('install', function (e) {
-    e.waitUntil(
-      caches.open(CACHE_NAME).then(function (cache) {
+// Cache resources
+self.addEventListener("install", function(event) {
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(function(cache) {
         console.log('installing cache : ' + CACHE_NAME)
-        return cache.addAll(FILES_TO_CACHE)
+        return cache.addAll(FILES_TO_CACHE);
       })
-    )
-  })
+    );
+    self.skipWaiting();
+  });
+  
 
+  // delete outdated caches
   self.addEventListener('activate', function(e) {
     e.waitUntil(
       caches.keys().then(function(keyList) {
@@ -49,22 +53,60 @@ const FILES_TO_CACHE = [
         );
       })
     );
+    // self.clients.claim();
   });
 
-  self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-      caches.match(e.request).then(function (request) {
-        if (request) { // if cache is available, respond with cache
-          console.log('responding with cache : ' + e.request.url)
-          return request
-        } else {       // if there are no cache, try fetching request
-          console.log('file is not cached, fetching : ' + e.request.url)
-          return fetch(e.request)
-        }
+  // respond with cache resources
+//   self.addEventListener('fetch', function (e) {
+//     console.log('fetch request : ' + e.request.url)
+//     e.respondWith(
+//       caches.match(e.request).then(function (request) {
+//         if (request) { // if cache is available, respond with cache
+//           console.log('responding with cache : ' + e.request.url)
+//           return request
+//         } else {       // if there are no cache, try fetching request
+//           console.log('file is not cached, fetching : ' + e.request.url)
+//           return fetch(e.request)
+//         }
   
-        // You can omit if/else for console.log & put one line below like this too.
-        // return request || fetch(e.request)
-      })
-    )
-  })
+//         // You can omit if/else for console.log & put one line below like this too.
+//         // return request || fetch(e.request)
+//       })
+//     )
+//   })
+
+self.addEventListener("fetch", function(event) {
+    // cache all get requests to /api routes
+    if (event.request.url.includes("/api/")) {
+      event.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(event.request);
+            });
+        }).catch(err => console.log(err))
+      );
+      return;
+    }
+
+    event.respondWith(
+        fetch(event.request).catch(function() {
+          return caches.match(event.request).then(function(response) {
+            if (response) {
+              return response;
+            } else if (event.request.headers.get("accept").includes("text/html")) {
+              // return the cached home page for all requests for html pages
+              return caches.match("/");
+            }
+          });
+        })
+      );
+    });
